936e321a0e6d8176a2ec0b064172424807b2b65c
bbovd <snake1982309@hotmail.com>
2014-04-07 15:01:47 +0300

    BB: Add UI style, master, home page

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        #region Controller methods
+
+        public ActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to Social Fund!";
+
+            return View();
+        }
+
+        #endregion
+    }
+}
--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -12,6 +12,17 @@ namespace SocialFund
     // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
