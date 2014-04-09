1919f1447b122560bb59cd968c441d5bea664f57
bbovd <snake1982309@hotmail.com>
2014-04-09 16:46:25 +0300

    BB: Add MailSender and unit tests for it

---
+++ Tests/MailSenderTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class EmailSenderTest
+    {
+        [TestMethod]
+        public void SendEmailWithAuthentication()
+        {
+            Assert.Inconclusive();
+
+            MailSender sender = new MailSender();
+            sender.SetCredential("snake@playasoftware.com", ",jdleq");
+            sender.InitializeMailMessage("test@mail.ru", "snake1982309@hotmail.com", "With Authentication", "Test");
+            sender.SendSmtp("smtp.gmail.com", 587, false);
+        }
+
+        [TestMethod] 
+        public void SendEmailWithoutAuthentication()
+        {
+            Assert.Inconclusive();
+
+            MailSender sender = new MailSender();
+            sender.InitializeMailMessage("test@mail.ru", "snake1982309@hotmail.com", "Without Authentication", "Test");
+            sender.SendSmtp("smtp.gmail.com", 587, false);
+        }
+
+        [TestMethod]
+        public void SendEmailSettingsFromConfiguration()
+        {
+            Assert.Inconclusive();
+
+            MailSender sender = new MailSender();
+            sender.SetCredential("snake@playasoftware.com", ",jdleq");
+            sender.InitializeMailMessage("test@mail.ru", "snake1982309@hotmail.com", "From Configuration", "Test");
+            sender.SendSmtp(false);
+        }
+    }
+}
