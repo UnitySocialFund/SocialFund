621701809c53d8963a9e0b90f7dbd39984fbd365
bbovd <snake1982309@hotmail.com>
2014-04-10 16:30:14 +0300

    BB: Add Contacts page with feedback information

---
+++ Web/Models/Contacts/FeedbackModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Common.Validation;
+
+namespace SocialFund.Models.Contacts
+{
+    public class FeedbackModel
+    {
+        [Required]
+        [ValidateStringLength(2, 50, false)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [ValidateStringLength(1, 250, false)]
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [ValidateStringLength(1, 2000, false)]
+        [DisplayName("Text")]
+        public string Text { get; set; }
+    }
+}
