30e9f1d3aaad29fe8d5935be270ce1cdda9dfbee
leontevaluda <leontevaluda@mail.ru>
2014-05-30 15:22:28 +0300

    LL: added file

---
+++ Web/Models/GroupViewModel/GroupIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialFund.Models.GroupViewModel
+{
+    public class GroupIndexViewModel
+    {
+        public GroupIndexViewModel()
+        {
+            this.MyGroups = new List<Group>();
+            this.InvitedGroup = new List<Group>();
+        }
+
+        public List<Group> MyGroups { get; set; }
+
+        public List<Group> InvitedGroup { get; set; }
+    }
+}
